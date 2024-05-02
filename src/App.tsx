5f816f3206964aa1e48977bd2ba5abd35ad1f642
AhmedYLasheen <ay514092@gmail.com>
2024-05-03 01:07:52 +0300

    install  and creating shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,18 @@ import NotFound from "./commponents/sharedCommponents/NotFound";
 import Login from "./commponents/AuthModule/Login/Login";
 import MasterLayout from "./commponents/DashboardModule/MasterLayout/MasterLayout";
 import Dashboard from "./commponents/DashboardModule/Dashbord/Dashboard";
-
-
+import InstructorGroups from "./commponents/DashboardModule/InstructorGroups/InstructorGroups";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <AuthLayout/>,
+      element: <AuthLayout />,
       errorElement: <NotFound />,
       children: [
         {
           index: true,
-          element: <Login/>,
+          element: <Login />,
         },
         {
           path: "login",
@@ -42,33 +41,23 @@ function App() {
     },
     {
       path: "dashboard",
-      element: (
-        
-          <MasterLayout />
-      
-      ),
+      element: <MasterLayout />,
       errorElement: <NotFound />,
       children: [
         {
           index: true,
           element: <Dashboard />,
         },
-
-
+        { path: "grops", element: <InstructorGroups /> },
       ],
     },
-  
-  
-  
-  
-  
-  ])
+  ]);
 
   return (
     <>
-              <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/commponents/DashboardModule/MasterLayout/MasterLayout.tsx
+++ src/commponents/DashboardModule/MasterLayout/MasterLayout.tsx
@@ -1,17 +1,19 @@
 import { Outlet } from "react-router-dom";
-
+import Sidebar from "../../sharedCommponents/Sidebar";
+import Navbar from "../../sharedCommponents/Navbar";
 
 const MasterLayout = () => {
-    return (
-   <div>
-         <div>
-             navbar 
-         </div>
-         <div>
-             <Outlet/>
-         </div>
-   </div>
-    );
-}
+  return (
+    <div className="flex">
+      <div className="	 ">
+        <Sidebar />
+      </div>
+      <div className=" w-full ">
+        <Navbar />
+        <Outlet />
+      </div>
+    </div>
+  );
+};
 
 export default MasterLayout;
